7dc4a4ce8101cfb6213cbdd296d1f2111004f4e5
joanmontas <joanmontas1@gmail.com>
2024-06-11 21:30:40 -0400

    Lexical Analysis

---
+++ Lexer/Lexer.c
@@ -0,0 +1,183 @@
+// Copyright Joan Montas
+// All rights reserved.
+// License under GNU General Public License v3.0
+
+#include "Lexer.h"
+
+Lexer Lexer_init(char *input_str)
+{
+	// ZERO element initialized
+	Lexer lex;
+	lex.input_string = input_str;
+	lex.input_size = 0u;
+	lex.position = 0u;
+	lex.current_char = '\0';
+	lex.err = 0;
+
+	token_set_from_char_array(&(lex.current_token), TOKEN_NONE_TYPE,
+				  TOKEN_NONE_TYPE);
+	token_set_from_char_array(&(lex.peek_token), TOKEN_NONE_TYPE,
+				  TOKEN_NONE_TYPE);
+
+	if (input_str == NULL) {
+		fprintf(stderr,
+			"ERROR: Lexer_init error. Given NULL instead of char*\n");
+		token_reset(&(lex.current_token));
+		token_reset(&(lex.peek_token));
+		token_set_from_char_array(&(lex.current_token),
+					  TOKEN_LEXER_ERROR_TYPE,
+					  TOKEN_LEXER_ERROR_TYPE);
+		token_set_from_char_array(&(lex.peek_token),
+					  TOKEN_LEXER_ERROR_TYPE,
+					  TOKEN_LEXER_ERROR_TYPE);
+		lex.err = 1;
+		return lex;
+	};
+
+	lex.input_string = input_str;
+	lex.input_size = strlen(input_str);
+
+	if (lex.input_size == 0) {
+		lex.current_char = '\0';
+	} else {
+		lex.current_char = lex.input_string[0];
+	}
+
+	return lex;
+}
+
+size_t lexer_next_token(Lexer *lex)
+{
+	char c = '\0';
+	String *s = string_init_default();
+
+	if (lex == NULL) {
+		fprintf(stderr,
+			"ERROR: Lexer_next_token error. Given NULL instead of Lexer\n");
+		return 0u;
+	}
+
+	if (strcmp(lex->current_token.token_type->c_string,
+		   TOKEN_LEXER_ERROR_TYPE) == 0) {
+		fprintf(stderr,
+			"ERROR: Lexer_next_token error. Current token contains a lexical error\n");
+		return 0u;
+	}
+
+	if (strcmp(lex->current_token.token_type->c_string, TOKEN_EOF_TYPE) ==
+	    0) {
+		// NOTE(Joan) Could set peek_token to EOF too - Joan
+		return 1u;
+	}
+
+	// peek_token becomes new
+	token_reset(&(lex->current_token));
+	lex->current_token = lex->peek_token;
+
+	lexer_eat_white_space(lex);
+
+	c = lex->current_char;
+
+	if (c == '\0') {
+		token_set_from_char_array(&(lex->peek_token), TOKEN_EOF_TYPE,
+					  TOKEN_EOF_TYPE);
+		string_destroy(s);
+		return 1u;
+	} else if (c == '"') {
+		lexer_next_character(lex);
+		c = lex->current_char;
+		while (1) {
+			if (c == '"') {
+				token_set_from_char_array_and_string(
+					&(lex->peek_token), TOKEN_QUOTED_TYPE,
+					s);
+				lexer_next_character(lex);
+				return 1u;
+
+			} else if (c == '\0') {
+				fprintf(stderr,
+					"ERROR: Lexer_next_token error. Quote not terminated\n");
+				token_set_from_char_array(
+					&(lex->peek_token),
+					TOKEN_LEXER_ERROR_TYPE,
+					TOKEN_LEXER_ERROR_TYPE);
+				lexer_next_character(lex);
+				string_destroy(s);
+				lex->err = 1;
+				return 0u;
+			}
+			string_append_char(s, c);
+			lexer_next_character(lex);
+			c = lex->current_char;
+		}
+
+	} else if (is_delimeter(c)) {
+		string_append_char(s, c);
+		token_set_from_char_array_and_string(&(lex->peek_token),
+						     TOKEN_SYMBOL_TYPE, s);
+		lexer_next_character(lex);
+		return 1u;
+
+	} else {
+		while (1) {
+			if (c == '\0' || c == ' ') {
+				token_set_from_char_array_and_string(
+					&(lex->peek_token), TOKEN_IDENT_TYPE,
+					s);
+				return 1u;
+			}
+			string_append_char(s, c);
+			lexer_next_character(lex);
+			c = lex->current_char;
+		}
+	}
+	string_destroy(s);
+	return 1u;
+}
+
+void lexer_next_character(Lexer *lex)
+{
+	if (lex == NULL) {
+		fprintf(stderr,
+			"ERROR: Lexer_next_character error. Given NULL instead of Lexer\n");
+		return;
+	}
+	lex->position++;
+	if (lex->position >= lex->input_size) {
+		lex->position--;
+		lex->current_char = '\0';
+		return;
+	}
+	lex->current_char = lex->input_string[lex->position];
+}
+
+void lexer_eat_white_space(Lexer *lex)
+{
+	// eats all whites space and new line character
+	char c = '\0';
+	if (lex == NULL) {
+		fprintf(stderr,
+			"ERROR: Lexer_eat_white_space error. Given NULL instead of Lexer\n");
+		return;
+	}
+
+	while (1) {
+		c = lex->current_char;
+		if (c == ' ' || c == '\n' || c == '\t') {
+			lexer_next_character(lex);
+		} else {
+			return;
+		}
+	}
+}
+
+void lexer_destroy(Lexer *l)
+{
+	if (l == NULL) {
+		fprintf(stderr,
+			"ERROR: lexer_destroy error. Received NULL instead of Lexer*\n");
+		return;
+	};
+	token_reset(&(l->current_token));
+	token_reset(&(l->peek_token));
+}
---
+++ Lexer/Lexer.h
@@ -0,0 +1,40 @@
+// Copyright Joan Montas
+// All rights reserved.
+// License under GNU General Public License v3.0
+
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "../Token/Token.h"
+#include "../String/String.h"
+
+typedef struct Lexer {
+	char *input_string;
+	size_t input_size;
+	size_t position;
+	char current_char;
+	Token current_token;
+	Token peek_token;
+	int err;
+} Lexer;
+
+// // constructor and destructor
+Lexer Lexer_init(char *input_str);
+void lexer_destroy(Lexer *l);
+// // get token in sequence
+size_t lexer_next_token(Lexer *lex);
+// // helpers
+void lexer_next_character(Lexer *lex);
+void lexer_eat_white_space(Lexer *lex);
+
+// char peekNextCharacter(Lexer *lex);
+	// // extracting IDENT and or keywords
+// char *extractIDENT(Lexer *lex);
+// char *extractINT(Lexer *lex);
+// Token extractString(Lexer *lex);
+
+#endif // LEXER_H
